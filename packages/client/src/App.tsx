0e71255dbd0da5b97df2d7105d717b1055a9cf12
lukekeum <lukekeum11@gmail.com>
2021-02-10 01:07:37 +0900

    Setup router

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-export default function App() {
-  return <div></div>;
+import Main from './pages/Main';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Main />
+        </Route>
+        <Route exact path='/login'>
+          <Login />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
+
+export default App;

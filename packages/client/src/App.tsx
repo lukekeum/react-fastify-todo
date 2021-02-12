3c6e7d3e73948fdda399cd6108774f48475eeb6d
lukekeum <lukekeum11@gmail.com>
2021-02-12 19:19:55 +0900

    Axios withCredentials to true

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { signInState as signInAtom } from './atoms/auth';
+import axios from 'axios';
 
 import Main from './pages/Main';
 import Login from './pages/Login';
+
+axios.defaults.withCredentials = true;
 
 function App() {
   const { isSignedIn } = useRecoilValue(signInAtom);

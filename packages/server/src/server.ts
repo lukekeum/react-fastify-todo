acc96b13ffeb7a1dc98057d5d9c6dfbbea2ba17b
lukekeum <lukekeum11@gmail.com>
2021-02-16 13:03:40 +0900

    Make server start after database connect

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,7 +2,7 @@ import './config/dotenv';
 import app from './app';
 import Database from './database';
 
-app.start(); // Start fastify server
+app.start();
 
 // Connect to mongoDB with mongoose
 const database = new Database();
